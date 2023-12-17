d3471b344d35205c088716556278c2ba9e6aad3a
Chanuka Sandaruwan Warnasooriya <warnasooriyacs1000@gmail.com>
2023-12-17 15:53:21 +0530

    Finished

--- src/Components/Account Section/Account.jsx
+++ src/Components/Account Section/Account.jsx
@@ -1,12 +1,56 @@
 import React from 'react'
 import './account.css';
+import { PiArrowSquareRightBold } from "react-icons/pi";
+import { BsThreeDots } from "react-icons/bs";
+import { FaRegEdit } from "react-icons/fa";
 
+//importing images
+import user1 from '../../Assets/user1.png';
 
 
 const Account = () => {
   return (
-    <div>
-      Account
+    <div className="account">
+      <div className="icons flex">
+        < PiArrowSquareRightBold className='icon'/>
+        <BsThreeDots className='icon'/>
+      </div>
+
+
+      <div className="accountDetails">
+        <div className="imageDiv">
+          <img src={user1} alt="account user image" />
+          <span className='name'>Sandaruwan Warnasooriya</span>
+          <span className='position'>Undergraduate Student</span>
+          <button className='btn flex'>
+            <FaRegEdit  className='icon'/>
+            Edit Profile
+          </button>
+        </div>
+
+
+        <div className="hoursDiv">
+          <span className='title'>Working Hours</span>
+          <div className="hoursCard flex">
+
+            <div className="right bg">
+              <span>Working Starts</span>
+              <h6>09.00 AM</h6>
+            </div>
+
+            <div className="left nobg">
+              <span>Working Ends</span>
+              <h6>05.00 PM</h6>
+            </div>
+
+          </div>
+        </div>
+
+      
+
+        <h1 className='city'>Colombo North</h1>
+        <span className="region">Sri Lanka GMT +8</span>
+      </div>
     </div>
   )
 }
--- src/Components/Body Section/BottomSection/Bottom.jsx
+++ src/Components/Body Section/BottomSection/Bottom.jsx
@@ -1,11 +1,118 @@
-import React from 'react'
+import React, {useState}from 'react'
 import './bottom.css';
-
+import Chart from 'react-apexcharts'
+import img from '../../../Assets/laptop.png';
+import img2 from '../../../Assets/png1.png';
+import img3 from '../../../Assets/newlogo.png';
 
 const Bottom = () => {
+
+  const [state, setState] = useState({
+    options:{
+      colors: ['#015658', '#5ABA8A'],
+      chart:{
+        id: 'basic-bar',
+        toolbar:{
+          show:true,
+          tools: {
+            selection: false,
+            zoom: false,
+            zoomin:false,
+            zoomout:false,
+            pan:false,
+            reset:true | '<img src="/static/icons/reset.png" width="10">',
+            customIcons:[]
+
+          }
+        },
+      },
+
+      xaxis:{
+        categories:['Sun', 'Mon','Tue','Wed','Thur', 'Fri','Sat']
+
+      }
+    },
+
+    series:[
+      {
+        name:'Updates',
+        data:[25, 33, 13, 89, 34, 20, 30]
+      },
+      {
+        name:'New Tasks',
+        data:[70, 47, 72, 45, 12, 59, 38]
+      }
+    ]
+
+
+  })
+
+
+
+
   return (
-    <div>
-      This is bottom section
+    <div className='bottom flex'>
+      <div className="graphDiv">
+        <span className='title'>All Activities(Graphical View)
+        </span>
+
+        <Chart options={state.options} 
+        series={state.series}
+        type='area' width='500'/>
+
+
+      </div>
+
+
+      <div className="scheduleDiv">
+        <span className='title'>Upcoming Schedule</span>
+
+        <div className="singleTask flex">
+          <div className="imgDiv flex">
+            <img src={img} alt='Task Image'/>
+            
+          </div>
+
+          <div className="taskInfo">
+            <span className='task'>Desk Time Redesign</span>
+            <span className='status'>Working On</span>
+          </div>
+          <span className='time'>09.30 AM</span>
+        </div>
+
+        <div className="singleTask flex">
+          <div className="imgDiv flex">
+            <img src={img2} alt='Task Image'/>
+            
+          </div>
+
+          <div className="taskInfo">
+            <span className='task'>New Landing Page</span>
+            <span className='status'>Working On</span>
+          </div>
+          <span className='time'>10.20 AM</span>
+        </div>
+
+
+        <div className="singleTask flex">
+          <div className="imgDiv flex">
+            <img src={img3} alt='Task Image'/>
+            
+          </div>
+
+          <div className="taskInfo">
+            <span className='task'>Create Animation</span>
+            <span className='status'>Working On</span>
+          </div>
+          <span className='time'>11.45 AM</span>
+        </div>
+
+
+        <div className="btn">
+          See All Activities
+        </div>
+      </div>
+      
     </div>
   )
 }
